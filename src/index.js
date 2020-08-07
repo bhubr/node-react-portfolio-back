048fa8ac038029f351e5adc7c5853410199a81ce
Benoit Hubert <benoithubert@gmail.com>
2020-08-07 17:39:45 +0200

    Add script to auto-fix ESLint errors

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+app.listen(port, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`Server listening on ${port}`);
+  }
+});
