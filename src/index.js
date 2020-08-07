89bc85d6fd7f4ce876974588cede82f787e58f78
Benoit Hubert <benoithubert@gmail.com>
2020-08-07 17:49:07 +0200

    Log to console using debug module

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const debug = require('debug');
 
 const app = express();
+const log = debug('express');
 
 const port = process.env.PORT || 8000;
 app.listen(port, (err) => {
   if (err) {
-    console.error(err);
+    log(err);
   } else {
-    console.log(`Server listening on ${port}`);
+    log(`Server listening on ${port}`);
   }
 });
